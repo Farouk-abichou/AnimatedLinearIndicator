db98d08942bfe0d0d22ba5967dc84a88f241e289
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-03-18 19:25:47 +0100

    Some Changes

--- app/src/main/java/com/example/animatedlinearindicator/MainActivity.kt
+++ app/src/main/java/com/example/animatedlinearindicator/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.animatedlinearindicator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animatedlinearindicator.ui.theme.AnimatedLinearIndicatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AnimatedLinearIndicatorTheme {
-        Greeting("Android")
+        AnimatedLinearIndicator()
+    }
+}
+
+@Composable
+fun AnimatedLinearIndicator() {
+
+    val width = 100.dp
+    val height = 400.dp
+    val radius = 50.dp
+    val pxValue = with(LocalDensity.current) { radius.toPx() }
+
+
+    Canvas(
+        modifier = Modifier
+            .width(width)
+            .height(height)
+
+    ) {
+        drawCircle(
+            color = Color.Cyan,
+            radius = pxValue,
+
+        )
     }
 }
