d2275af2a5ae7a1c0e7c431f376fe8c87ded9067
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-03-18 22:40:19 +0100

    Some Changes

--- app/src/main/java/com/example/animatedlinearindicator/MainActivity.kt
+++ app/src/main/java/com/example/animatedlinearindicator/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.progressSemantics
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.example.animatedlinearindicator.ui.theme.AnimatedLinearIndicatorTheme
 
@@ -84,5 +89,60 @@ fun AnimatedLinearIndicator() {
         end = Offset(pxWidthValue/2,pxRadiusValue*6),
         progress = 0.0f,
         color = Color.Black
+    )
+
+
+
+
+    Canvas(
+        Modifier
+            .progressSemantics(0.0F)
+            .size(LinearIndicatorWidth, LinearIndicatorHeight)
+    ) {
+
+        val strokeWidth = size.height
+        drawLinearIndicatorTrack(
+            start = start,
+            end = end,
+            color = color,
+            strokeWidth =  strokeWidth,
+        )
+        drawLinearIndicator(
+            start = start,
+            end = Offset(progress,end.y),
+            color = color,
+            strokeWidth =  strokeWidth,
+        )
+
+    }
+}
+
+
+
+
+
+
+
+private fun DrawScope.drawLinearIndicator(
+    start: Offset,
+    end: Offset,
+    color: Color,
+    strokeWidth: Float,
+) {
+    val width = size.width
+    val height = size.height
+    // Start drawing from the vertical center of the stroke
+
+    val isLtr = layoutDirection == LayoutDirection.Ltr
+    val barStart = (if (isLtr) start.x else 1f - end.x) * width
+    val barEnd = (if (isLtr) end.x else 1f - start.x) * width
+
+    // Progress line
+    drawLine(
+        color = color,
+        start = start,
+        end = end,
+        strokeWidth = strokeWidth,
+        cap = StrokeCap.Round,
     )
 }
