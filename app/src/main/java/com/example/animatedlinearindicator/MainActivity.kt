fe3c5b6514b69624469691ab29cb00871c9686b4
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-03-18 19:53:00 +0100

    Some Changes

--- app/src/main/java/com/example/animatedlinearindicator/MainActivity.kt
+++ app/src/main/java/com/example/animatedlinearindicator/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -83,4 +79,8 @@ fun AnimatedLinearIndicator() {
             center = Offset(pxWidthValue/2, pxRadiusValue*7)
         )
     }
+    CustomLinearProgressIndicator(
+        start = pxRadiusValue*2,
+        end = pxRadiusValue*6,
+    )
 }
---
+++ app/src/main/java/com/example/animatedlinearindicator/RoundedCornerLinearProgressIndicator.kt
@@ -0,0 +1,90 @@
+package com.example.animatedlinearindicator
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.progressSemantics
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun CustomLinearProgressIndicator(
+    start:  Float,
+    end :Float,
+    color: Color = Color.Gray,
+    trackColor: Color = Color.White,
+) {
+
+    Canvas(
+        Modifier
+            .progressSemantics(end)
+            .size(LinearIndicatorWidth, LinearIndicatorHeight)
+    ) {
+
+        val strokeWidth = size.height
+        drawLinearIndicatorTrack(trackColor, strokeWidth)
+        drawLinearIndicator(
+            startFraction = start,
+            endFraction = end,
+            color = color,
+            strokeWidth =  strokeWidth,
+            yOffset = strokeWidth/2
+        )
+
+    }
+}
+
+
+internal val LinearIndicatorWidth = 240.dp
+
+internal val LinearIndicatorHeight = LinearProgressIndicatorTokens.TrackHeight
+
+
+internal object LinearProgressIndicatorTokens {
+    val TrackHeight = 4.0.dp
+}
+
+private fun DrawScope.drawLinearIndicatorTrack(
+    color: Color,
+    strokeWidth: Float
+) {
+    val height = size.height
+    drawLinearIndicator(0f, 1f, color, strokeWidth,height / 2)
+}
+
+
+
+private fun DrawScope.drawLinearIndicator(
+    startFraction: Float,
+    endFraction: Float,
+    color: Color,
+    strokeWidth: Float,
+    yOffset : Float
+) {
+    val width = size.width
+    val height = size.height
+    // Start drawing from the vertical center of the stroke
+    val yOffset = yOffset
+
+    val isLtr = layoutDirection == LayoutDirection.Ltr
+    val barStart = (if (isLtr) startFraction else 1f - endFraction) * width
+    val barEnd = (if (isLtr) endFraction else 1f - startFraction) * width
+
+    // Progress line
+    drawLine(
+        color,
+        Offset(barStart, yOffset),
+        Offset(barEnd, yOffset),
+        strokeWidth,
+        cap = StrokeCap.Round,
+
+        )
+
+}
